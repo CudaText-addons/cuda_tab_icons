e4320206a8049f20d42885eab512c919a1eb3b90
halfbrained <zulufaka.ms@gmail.com>
2021-03-30 19:57:10 +0300

    add per-document user-chosen icons

--- __init__.py
+++ __init__.py
@@ -3,12 +3,25 @@ import re
 import json
 from cudatext import *
 
-FN_INI = 'plugins.ini'
+fn_config = os.path.join(app_path(APP_DIR_SETTINGS), 'cuda_tab_icons.json')
+icons_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'misc_icons')
+
+USER_DIR = os.path.expanduser('~')
+
+def collapse_path(path):
+    if (path + os.sep).startswith(USER_DIR + os.sep):
+        path = path.replace(USER_DIR, '~', 1)
+    return path
 
 class Command:
     def __init__(self):
 
+        self.icon_theme = 'vscode_16x16'
+        self.show_lex_icons = True
+        
+        self.misc_icon_map = {} # path -> icon name
         self.imglist = app_proc(PROC_GET_TAB_IMAGELIST, '')
+        
         self.load_options()
 
         try:
@@ -27,6 +40,9 @@ class Command:
         self.icon_json = os.path.join(self.icon_dir, 'icons.json')
         self.icon_json_dict = json.loads(open(self.icon_json).read())
         self.icon_indexes = {}
+        self.misc_icon_indexes = {}
+        
+        menu_proc('tab', MENU_ADD, command='cuda_tab_icons.iconify_current', caption='Set tab icon...')
 
 
     def icon_get(self, key, icon_def):
@@ -40,21 +56,41 @@ class Command:
             n = icon_def
             self.icon_indexes[key] = n
             return n
-
+            
         fn = os.path.join(self.icon_dir, fn)
-        n = imagelist_proc(self.imglist, IMAGELIST_ADD, value=fn)
+        n = self.icon_load(fn, icon_def, key, self.icon_indexes)
+        return n
+
+
+    def icon_get_misc(self, ic_name):
+        
+        if ic_name not in self.misc_icon_indexes:
+            # allow absolute path to icon in config, "advanced feature"
+            ic_path = os.path.join(icons_path, ic_name)  if os.sep not in ic_name else  ic_name 
+            self.icon_load(ic_path, -1, ic_name, self.misc_icon_indexes)
+      
+        return self.misc_icon_indexes[ic_name]
+        
+    def icon_load(self, ic_path, icon_def, key, cache):
+        
+        n = imagelist_proc(self.imglist, IMAGELIST_ADD, value=ic_path)
         if n is None:
-            print('Incorrect filetype icon:', fn)
+            print('Incorrect filetype icon:', ic_path)
             n = icon_def
-        self.icon_indexes[key] = n
+        cache[key] = n
         return n
 
 
     def update_icon(self, ed_self, is_picture):
 
+        filename = ed_self.get_filename()
         icon = -1
+        lexer = None
         if is_picture:
             lexer = '_img'
+        elif filename and filename in self.misc_icon_map:
+            ic_name = self.misc_icon_map[filename]
+            icon = self.icon_get_misc(ic_name)
         else:
             lexer = ed_self.get_prop(PROP_LEXER_FILE)
             #support lite lexers
@@ -75,17 +111,103 @@ class Command:
 
         self.update_icon(ed_self, False)
 
+    def iconify_current(self):
+        
+        path = ed.get_filename()
+        if path:
+            ic_fns = [name for name in os.listdir(icons_path)  if name.lower().endswith('.png')]
+            ic_fns.sort(key=lambda n: icon_aliases.get(n, n))
+            ic_names_aliased = [icon_aliases.get(name, name) for name in ic_fns]
+            ic_names_noext = [os.path.splitext(name)[0] for name in ic_names_aliased] # no exts
+            
+            doc_fn = os.path.basename(path)
+            ic_ind = dlg_menu(DMENU_LIST, ic_names_noext, caption='Choose icon for:\n  '+doc_fn)
+            
+            if ic_ind is not None:
+                ic_name = ic_fns[ic_ind]
+                imind = self.icon_get_misc(ic_name)
+                ed.set_prop(PROP_TAB_ICON, imind)
+                
+                self.misc_icon_map[path] = ic_name
+                self.save_options()
+
+    def clear_current(self):
+        
+        path = ed.get_filename()
+        if path  and path in self.misc_icon_map:
+            del self.misc_icon_map[path]
+      
+        ed.set_prop(PROP_TAB_ICON, -1)
+        self.save_options()
+
     def config(self):
 
         self.save_options()
 
-        fn = app_path(APP_DIR_SETTINGS)+os.sep+FN_INI
-        file_open(fn)
+        file_open(fn_config)
 
     def save_options(self):
-
-        ini_write(FN_INI, 'tab_icons', 'icons', self.icon_theme)
+        
+        collapsed = {collapse_path(path):ic_name for path,ic_name in self.misc_icon_map.items()}
+        cfg = {
+            'icon_theme': self.icon_theme,  
+            'show_lex_icons': self.show_lex_icons,
+            'custom_icons_map': collapsed,
+        }
+        
+        with open(fn_config, 'w', encoding='utf-8') as f:
+            json.dump(cfg, f, indent=2)
 
     def load_options(self):
+        if os.path.exists(fn_config):
+            with open(fn_config, 'r', encoding='utf-8') as f:
+                j = json.load(f)
+                
+            self.icon_theme = j.get('icon_theme', self.icon_theme)
+            self.show_lex_icons = j.get('show_lex_icons', self.show_lex_icons)
+            
+            mp = j.get('custom_icons_map', self.misc_icon_map)
+            self.misc_icon_map = {os.path.expanduser(path):ic_name for path,ic_name in mp.items()}
+        
 
-        self.icon_theme = ini_read(FN_INI, 'tab_icons', 'icons', 'vscode_16x16')
+icon_aliases = {
+    'appointment-soon.png': 'clock',
+    #'audio-headphones.png': '',
+    #'audio-speakers.png': '',
+    'avatar-default.png': 'avatar',
+    'changes-prevent.png': 'lock',
+    #'computer.png': '',
+    #'drive-multidisk.png': '',
+    'edit-delete.png': 'delete',
+    'edit-find.png': 'search purple',
+    'emblem-default.png': 'success',
+    'emblem-downloads.png': 'download',
+    'emblem-mail.png': 'mail',
+    'emblem-readonly.png': 'lock2',
+    'emblem-unreadable.png': 'X',
+    'emblem-web.png': 'web',
+    'help-faq.png': '???',
+    #'input-dialpad.png': '',
+    'input-tablet.png': 'edit',
+    'mail-attachment.png': 'attachment',
+    'media-playback-start.png': 'start',
+    'media-playback-stop.png': 'stop',
+    'network-vpn.png': 'lock3',
+    'non-starred.png': 'star empty',
+    'preferences-desktop-font.png': 'alphabet',
+    'preferences-system-privacy.png': 'privacy',
+    'preferences-system-sharing.png': 'sharing',
+    'security-high.png': 'shield green',
+    'security-low.png': 'shield yellow',
+    'security-medium.png': 'shield grey',
+    'software-update-available.png': 'orange thingy',
+    'software-update-urgent.png': 'warning',
+    'starred.png': 'star',
+    'system-file-manager.png': 'storage',
+    'system-search.png': 'search yellow',
+    #'trophy-bronze.png': '',
+    #'trophy-gold.png': '',
+    #'trophy-silver.png': '',
+    'zoom-in.png': 'plus',
+    'zoom-out.png': 'minus',
+}
--- readme/readme.txt
+++ readme/readme.txt
@@ -9,5 +9,13 @@ to the name of subfolder in "data/filetypeicons".
 for themes with bigger icon size (e.g. 24x24) you will need to adjust CudaText option
 "ui_tab_size_y", to make UI tabs height bigger.
 
+adds comamnds to editor's tab context menu to set custom document-specific icon 
+from serveral existing. additional icons can be added to the 'icons' directory in 
+plugin's folder: py/cuda_tab_icons, dimensions should be same as of the theme.
+
+also custom icons can be set or reset from the main menu:
+Plugins > Tab Icons
+
+
 author: Alexey Torgashin (CudaText)
 license: MIT
